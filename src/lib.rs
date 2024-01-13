5daf6a662b5f6df15e7b7681b321a34a37cfd6d3
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-01-14 00:08:17 +0900

    Add `rights` function

--- src/lib.rs
+++ src/lib.rs
@@ -155,6 +155,60 @@ where
     }
 }
 
+pub struct Rights<'a, Left, Right>(Keys<'a, Rc<Right>, HashSet<Rc<Left>>>)
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone;
+
+impl<Left, Right> Clone for Rights<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    fn clone(&self) -> Self {
+        Rights(self.0.clone())
+    }
+}
+
+impl<Left, Right: Debug> fmt::Debug for Rights<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.0.clone()).finish()
+    }
+}
+
+impl<'a, Left, Right> Iterator for Rights<'a, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    type Item = &'a Right;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|x| &(**x))
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<Left, Right> ExactSizeIterator for Rights<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    #[inline]
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
 impl<Left, Right> Many2Many<Left, Right>
 where
     Left: Hash + Eq + Clone,
@@ -162,5 +216,9 @@ where
 {
     pub fn lefts(&self) -> Lefts<'_, Left, Right> {
         Lefts(self.left.keys())
+    }
+
+    pub fn rights(&self) -> Rights<'_, Left, Right> {
+        Rights(self.right.keys())
     }
 }
