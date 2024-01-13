84e119d6ed575a5bc0411e5c4da4574f6ffaab04
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-01-13 16:09:48 +0900

    Add `new` function

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,16 @@ where
     left: HashMap<Rc<Left>, HashSet<Rc<Right>>>,
     right: HashMap<Rc<Right>, HashSet<Rc<Left>>>,
 }
+
+impl<Left, Right> Many2Many<Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    pub fn new() -> Many2Many<Left, Right> {
+        Many2Many {
+            left: HashMap::new(),
+            right: HashMap::new(),
+        }
+    }
+}
