8aa16002ea1c0bb013c1ca1a2538b23dc6bee457
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-01-13 16:09:28 +0900

    Add `Many2Many` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+    rc::Rc,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Many2Many<Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    left: HashMap<Rc<Left>, HashSet<Rc<Right>>>,
+    right: HashMap<Rc<Right>, HashSet<Rc<Left>>>,
 }
