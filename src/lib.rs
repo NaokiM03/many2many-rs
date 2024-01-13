e8dc6f76dfefd0c87356f0890370f0b8eccdaf7e
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-01-14 00:00:19 +0900

    Add `lefts` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use core::fmt;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{hash_map::Keys, HashMap, HashSet},
+    fmt::Debug,
     hash::Hash,
     rc::Rc,
 };
@@ -96,5 +98,69 @@ where
         } else {
             None
         }
+    }
+}
+
+pub struct Lefts<'a, Left, Right>(Keys<'a, Rc<Left>, HashSet<Rc<Right>>>)
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone;
+
+impl<Left, Right> Clone for Lefts<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    fn clone(&self) -> Self {
+        Lefts(self.0.clone())
+    }
+}
+
+impl<Left: Debug, Right> fmt::Debug for Lefts<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.0.clone()).finish()
+    }
+}
+
+impl<'a, Left, Right> Iterator for Lefts<'a, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    type Item = &'a Left;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|x| &(**x))
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<Left, Right> ExactSizeIterator for Lefts<'_, Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    #[inline]
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<Left, Right> Many2Many<Left, Right>
+where
+    Left: Hash + Eq + Clone,
+    Right: Hash + Eq + Clone,
+{
+    pub fn lefts(&self) -> Lefts<'_, Left, Right> {
+        Lefts(self.left.keys())
     }
 }
